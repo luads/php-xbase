fcda9f0eb8dd1c12b655d56fedfd7e313ac388e4
Josef Moravec <josef.moravec@gmail.com>
2017-01-06 09:52:19 +0100

    Get the right filename for memo file in case somebody is using uppercased file names

--- src/XBase/Memo.php
+++ src/XBase/Memo.php
@@ -17,7 +17,7 @@ class Memo
 
     protected function open()
     {
-        $fileName = preg_replace("/dbf/", "fpt", $this->tableName);
+        $fileName = str_replace(["dbf", "DBF"], ["fpt", "FPT"], $this->tableName);
 
         if (!file_exists($fileName)) {
             return false;
