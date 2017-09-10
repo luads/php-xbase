686776bc09c026572ef570a1d1704d0780ec1f0d
Mohammad Saleh Salehi <msstrashbin@gmail.com>
2017-09-10 13:55:23 +0430

    Update write_dbf.php

--- example/write_dbf.php
+++ example/write_dbf.php
@@ -1,8 +1,7 @@
 <?php
 include "vendor/autoload.php";
-use ShpAdapter\ShapeFile;
-use \ShpAdapter\XBase\WritableTable;
-use \ShpAdapter\XBase\Record;
+use XBase\WritableTable;
+use XBase\Record;
 
 $newTable=WritableTable::create('test.dbf',[['test',Record::DBFFIELD_TYPE_CHAR,10]]);
 for($i=0;$i<100;$i++){
