068fb43294c6fd470ff172e9d1b8212e822ddb47
gam6itko <gam6itko@gmail.com>
2020-02-04 11:27:18 +0300

    tests

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -13,20 +13,20 @@ class SimpleTest extends TestCase
     {
         $filepath = __DIR__.'/Resources/cbr_072019b1.dbf';
 
-        $table = new Table($filepath, null, 'CP1251');
+        $table = new Table($filepath, null, 'cp866');
 
-        self::assertEquals(18, $table->getColumnCount());
-        self::assertEquals(10, $table->getRecordCount());
+        self::assertSame(18, $table->getColumnCount());
+        self::assertSame(10, $table->getRecordCount());
 
-        self::assertEquals(3, $table->version);
-        self::assertEquals(false, $table->foxpro);
-        self::assertEquals(1580774400, $table->modifyDate);
-        self::assertEquals(609, $table->headerLength);
-        self::assertEquals(225, $table->recordByteLength);
-        self::assertEquals(false, $table->inTransaction);
-        self::assertEquals(false, $table->encrypted);
-        self::assertEquals(0x00, bin2hex($table->mdxFlag));
-        self::assertEquals(0x00, bin2hex($table->languageCode));
+        self::assertSame(3, $table->version);
+        self::assertSame(false, $table->foxpro);
+        self::assertSame(1580774400, $table->modifyDate);
+        self::assertSame(609, $table->headerLength);
+        self::assertSame(225, $table->recordByteLength);
+        self::assertSame(false, $table->inTransaction);
+        self::assertSame(false, $table->encrypted);
+        self::assertSame('00', bin2hex($table->mdxFlag));
+        self::assertSame('00', bin2hex($table->languageCode));
 
         $columns = $table->getColumns();
         self::assertIsArray($columns);
@@ -35,27 +35,27 @@ class SimpleTest extends TestCase
         //<editor-fold desc="columns">
         $column = $columns['regn'];
         self::assertInstanceOf(Column::class, $column);
-        self::assertEquals('regn', $column->getName());
-        self::assertEquals(Record::DBFFIELD_TYPE_NUMERIC, $column->getType());
-        self::assertEquals(4, $column->getLength());
-        self::assertEquals(1, $column->getBytePos());
-        self::assertEquals(0, $column->getColIndex());
+        self::assertSame('regn', $column->getName());
+        self::assertSame(Record::DBFFIELD_TYPE_NUMERIC, $column->getType());
+        self::assertSame(4, $column->getLength());
+        self::assertSame(1, $column->getBytePos());
+        self::assertSame(0, $column->getColIndex());
 
         $column = $columns['plan'];
         self::assertInstanceOf(Column::class, $column);
-        self::assertEquals('plan', $column->getName());
-        self::assertEquals(Record::DBFFIELD_TYPE_CHAR, $column->getType());
-        self::assertEquals(1, $column->getLength());
-        self::assertEquals(5, $column->getBytePos());
-        self::assertEquals(1, $column->getColIndex());
+        self::assertSame('plan', $column->getName());
+        self::assertSame(Record::DBFFIELD_TYPE_CHAR, $column->getType());
+        self::assertSame(1, $column->getLength());
+        self::assertSame(5, $column->getBytePos());
+        self::assertSame(1, $column->getColIndex());
 
         $column = $columns['dt'];
         self::assertInstanceOf(Column::class, $column);
-        self::assertEquals('dt', $column->getName());
-        self::assertEquals(Record::DBFFIELD_TYPE_DATE, $column->getType());
-        self::assertEquals(8, $column->getLength());
-        self::assertEquals(216, $column->getBytePos());
-        self::assertEquals(16, $column->getColIndex());
+        self::assertSame('dt', $column->getName());
+        self::assertSame(Record::DBFFIELD_TYPE_DATE, $column->getType());
+        self::assertSame(8, $column->getLength());
+        self::assertSame(216, $column->getBytePos());
+        self::assertSame(16, $column->getColIndex());
 
         unset($column, $columns);
         //</editor-fold>
@@ -69,7 +69,7 @@ class SimpleTest extends TestCase
         $json = <<<JSON
 {
     "regn": 1,
-    "plan": "\u0402",
+    "plan": "\u0410",
     "num_sc": "10605",
     "a_p": "1",
     "vr": 223717,
@@ -89,6 +89,11 @@ class SimpleTest extends TestCase
 }
 JSON;
         self::assertJsonStringEqualsJsonString($json, json_encode($record->getData()));
-
+        self::assertSame(10605, $record->getNum('num_sc'));
+        self::assertSame('А', $record->getString('plan')); //cyrilic
+        self::assertSame(1564617600, $record->getDate('dt'));
+        self::assertSame(1564617600, $record->getObject($record->getColumn('dt')));
+        $dt = new \DateTime($record->forceGetString('dt'));
+        self::assertEquals('2019-08-01T00:00:00+00:00', $dt->format(DATE_W3C));
     }
 }
