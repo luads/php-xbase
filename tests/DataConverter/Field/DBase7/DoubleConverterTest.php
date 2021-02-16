63bf9584d3a2a38eb502df830bca1591db5b725a
Aaron Huisinga <aaronhuisinga@gmail.com>
2021-02-16 14:31:42 -0600

    Add support for DBase 7 Double type

---
+++ tests/DataConverter/Field/DBase7/DoubleConverterTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace XBase\Tests\Record\DataConverter\Field\DBase7;
+
+use PHPUnit\Framework\TestCase;
+use XBase\Column\ColumnInterface;
+use XBase\DataConverter\Field\DBase7\DoubleConverter;
+use XBase\Table;
+
+class DoubleConverterTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test(string $binaryString, float $float): void
+    {
+        $table = $this->createMock(Table::class);
+        $column = $this->createMock(ColumnInterface::class);
+        $converter = new DoubleConverter($table, $column);
+        self::assertSame($float, $converter->fromBinaryString($binaryString));
+        self::assertEquals($float, unpack('E', $converter->toBinaryString($float))[1]);
+    }
+
+    public function dataProvider()
+    {
+        yield [
+            base64_decode('P5cDMzMzMzI='),
+            -199.9,
+        ];
+
+        yield [
+            base64_decode('P61f//////8='),
+            -74.5,
+        ];
+
+        yield [
+            base64_decode('gAAAAAAAAAA='),
+            0.0,
+        ];
+        
+        yield [
+            base64_decode('wCIAAAAAAAA='),
+            9.0
+        ];
+
+        yield [
+            base64_decode('wFOMzMzMzM0='),
+            78.2,
+        ];
+    }
+}
