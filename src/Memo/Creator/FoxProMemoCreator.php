786db83a69cf1abb7cf2c40f1fb3664178dc4526
Eric Defore <eric.defore@gmail.com>
2022-09-22 09:45:12 -0400

    Ran php-cs-fixer fix on FoxProMemoCreator
    Referencing #118

--- src/Memo/Creator/FoxProMemoCreator.php
+++ src/Memo/Creator/FoxProMemoCreator.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace XBase\Memo\Creator;
 
@@ -13,12 +15,12 @@ class FoxProMemoCreator extends AbstractMemoCreator
 
     protected function writeHeader(Stream $stream): void
     {
-        $stream->write(pack('N',8)); //next block
+        $stream->write(pack('N', 8)); //next block
 
         $stream->seek(4);
-        $stream->write(str_pad('',2,chr(0))); //reserved
+        $stream->write(str_pad('', 2, chr(0))); //reserved
 
         $stream->seek(6);
-        $stream->write(pack('n',64)); //Block size. 8 * 64 == 512, which will start records after the header
+        $stream->write(pack('n', 64)); //Block size. 8 * 64 == 512, which will start records after the header
     }
 }
